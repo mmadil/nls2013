1c0da86fdc5d4b2111b3bf02bc99b8292530262d
mmadil14 <mmadil_14@yahoo.com>
2013-01-14 20:29:37 +0530

    login added

--- cashier.php
+++ cashier.php
@@ -6,6 +6,16 @@ include('includes/header.php')
 
 <h2> Cashier </h2>
 
+<div id="login">
+ <form action="login.php" method="POST">
+  <table>
+   <tr> <td> User Name: </td><td><input type="text" name="username" autocomplete="off"> </td> </tr>
+   <tr> <td> Password: </td><td><input type="password" name="passwd" autocomplete="off"> </td> </tr>
+   <td> <input type="submit" name="submit" value="Log in"> </td>
+  </table>
+ </form>
+</div>
+
 <?php
 
 include('includes/footer.php')
