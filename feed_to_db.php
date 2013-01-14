62f87cb27c21ecf58ffc1381e7bfe9b4bf0d2253
mmadil14 <mmadil_14@yahoo.com>
2013-01-14 20:30:10 +0530

    data insertion added

--- feed_to_db.php
+++ feed_to_db.php
@@ -12,40 +12,68 @@ include('includes/header.php');
 
 ?>
 
+<?php
+ if($_POST['submit'])
+ {
+  $item_name_ = $_POST['item_name'];
+  $price_ = $_POST['price'];
+  $stock_ = $_POST['stock'];
+
+  mysql_query("INSERT INTO list VALUES ('','$item_name_','$price_','$stock_')") or die(mysql_error());
+ }
+
+?>
+
 <h2> Insert desired items in the database </h2>
 
 
 <form action="feed_to_db.php" method="POST">
- 
- Item Name : <input type="text" name="item_name" autocomplete="off"><p>
- Price : <input type="text" name="price" autocomplete="off"><p>
- <input type="submit" name="submit" value="Insert Data">
-<?php 
-
-
-
-?>
-
+ <table>
+ <td>
+  Item Name : <input type="text" name="item_name" autocomplete="off">
+ </td>
+ <td>
+  Price : <input type="text" name="price" autocomplete="off">
+ </td>
+ <td> 
+  In Stock : <input type="text" name="stock" autocomplete="off">
+ </td>
+ <td>
+  <input type="submit" name="submit" value="Insert Data">
+ </td>
+ </table>
 </form>
 
-<div id="list">
- <?php
-  $data = mysql_query("SELECT * FROM list ORDER BY item_name ASC");
+ <ul>
+  <?php
   
-  while($row = mysql_fetch_assoc("$data"))
+  $extract = mysql_query("SELECT * FROM list ORDER BY item_name  ASC") or die(mysql_error());
+  $numrows = mysql_num_rows($extract);
+
+  echo "<br />";
+  echo "<table>
+    <td>Items</td>
+    <td>Price</td>
+    <td>Stock</td>";
+  while ($row = mysql_fetch_assoc($extract))	
    {
+	$item_name = $row['item_name'];
+	$price     = $row['price'];
+	$stock     = $row['stock'];
 
-   $id = $row['id'];
-   $item_name = $row['item_name'];
-   $price = $row['price'];
-
-   echo "TEST";
+  echo "
+    <tr>
+    <td>$item_name</td>
+    <td>$price</td>
+    <td>$stock</td>
+    </tr>
+    ";
 
    }
+  echo "</table>";
+
  ?>
-</div>
-
-
+</ul>
 
 <?php
 
