e60eba8353c5b86666f3f380cc23bf63fa617f94
mmadil14 <mmadil_14@yahoo.com>
2013-01-16 02:37:10 +0530

    finally completed

--- cashier.php
+++ cashier.php
@@ -13,24 +13,11 @@ include('includes/header.php')
 
 <h2> Cashier </h2>
 
-<div id="not-connected">
 <?php
 
- $username = $_POST['username'];
- $passwd = $_POST['passwd'];
-
- if($username && $passwd)
- {
-
-  $connect = mysql_connect("localhost","root","") or die("Couldn't connect!");
-  mysql_select_db("phplogin") or die("Couldn't find db");
-
- }
-  else
-   die("Please enter valid combination of username and password to continue!");
+include('includes/cashier_login.php');
 
 ?>
-</div>
 
 <?php
 
--- stalls.php
+++ stalls.php
@@ -8,21 +8,9 @@ include('includes/header.php')
 
 <?php
 
- $username = $_POST['username'];
- $passwd = $_POST['passwd'];
-
- if($username && $passwd)
- {
-
-  $connect = mysql_connect("localhost","root","") or die("Couldn't connect!");
-  mysql_select_db("phplogin") or die("Couldn't find db");
-
- }
-  else
-   die("Please enter valid combination of username and password to continue!");
+include('includes/stalls_login.php');
 
 ?>
-
 
 <?php
 
