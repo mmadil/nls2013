fd39664701ef322075025c2c3208b59af7cfb4f9
mmadil14 <mmadil_14@yahoo.com>
2013-01-20 22:52:33 +0530

    rechaging is done here

--- includes/recharge.php
+++ includes/recharge.php
@@ -1,8 +1,17 @@
 <?php
 
-require('connect.php');
+require('includes/connect.php');
 
 ?>
 
 
+<table>
+<div id='recharge'>
+ <form action='r.php' method='POST'>
+ <tr><td>Card Number : <input type='text' name='card_no'><br /></td></tr>
+ <tr><td>Add balance : <input type='text' name='balance'><br /></td></tr>
+ <tr><td><input type="submit" name='submit' value="Credit Value"></td></tr>
+ </form> 
+</div> 
+</table>
 
