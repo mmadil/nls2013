f48a57a6d8fe9c5064bc9b82cd40333df3b9dee0
mmadil14 <mmadil_14@yahoo.com>
2013-01-25 21:46:56 +0530

    recharge functionality completed

--- includes/recharge.php
+++ includes/recharge.php
@@ -1,17 +1,9 @@
-<?php
-
-require('includes/connect.php');
-
-?>
-
-
+<div id="recharge">
 <table>
-<div id='recharge'>
  <form action='r.php' method='POST'>
- <tr><td>Card Number : <input type='text' name='card_no'><br /></td></tr>
- <tr><td>Add balance : <input type='text' name='balance'><br /></td></tr>
+ <tr><td>Card Number : <input type='text' name='card_no' autocomplete="off"></td></tr>
+ <tr><td>Add balance : <input type='text' name='balance' autocomplete="off"></td></tr>
  <tr><td><input type="submit" name='submit' value="Credit Value"></td></tr>
  </form> 
-</div> 
 </table>
-
+</div>
