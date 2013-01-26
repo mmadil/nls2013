3c96621569fabd33a37bdde6e43d555c3d8676c4
mmadil14 <mmadil_14@yahoo.com>
2013-01-26 23:54:53 +0530

    data feeder updated

--- feed_to_db.php
+++ feed_to_db.php
@@ -15,11 +15,12 @@ include('includes/header.php');
 <?php
  if($_POST['submit'])
  {
+  $item_no_ = $_POST['item_no'];
   $item_name_ = $_POST['item_name'];
   $price_ = $_POST['price'];
   $stock_ = $_POST['stock'];
 
-  mysql_query("INSERT INTO list VALUES ('','$item_name_','$price_','$stock_')") or die(mysql_error());
+  mysql_query("INSERT INTO list VALUES ('','$item_no_','$item_name_','$price_','$stock_')") or die(mysql_error());
  }
 
 ?>
@@ -29,6 +30,9 @@ include('includes/header.php');
 
 <form action="feed_to_db.php" method="POST">
  <table>
+ <td>
+  Item Number : <input type="text" name="item_no" autocomplete="off">
+ </td>
  <td>
   Item Name : <input type="text" name="item_name" autocomplete="off">
  </td>
@@ -49,22 +53,25 @@ include('includes/header.php');
  <ul>
   <?php
   
-  $extract = mysql_query("SELECT * FROM list ORDER BY item_name  ASC") or die(mysql_error());
+  $extract = mysql_query("SELECT * FROM list ORDER BY item_no  ASC") or die(mysql_error());
   $numrows = mysql_num_rows($extract);
 
   echo "<br />";
   echo "<table>
+    <td>Item Number</td>
     <td>Items</td>
     <td>Price</td>
     <td>Stock</td>";
   while ($row = mysql_fetch_assoc($extract))	
    {
+        $item_no   = $row['item_no'];
 	$item_name = $row['item_name'];
 	$price     = $row['price'];
 	$stock     = $row['stock'];
 
   echo "
     <tr>
+    <td>$item_no</td>
     <td>$item_name</td>
     <td>$price</td>
     <td>$stock</td>
